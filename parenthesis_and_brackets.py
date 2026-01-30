46a47093c7b900249f43d6aa01c7ba501c2a2c94
sulfobus <32538694+sulfobus@users.noreply.github.com>
2026-01-30 21:28:46 +0000

    First commit

---
+++ parenthesis_and_brackets.py
@@ -0,0 +1,50 @@
+# This script checks if a string of parentheses and brackets is balanced and properly nested.
+# Enter a string containing parentheses and brackets
+print("Enter a string containing parentheses and brackets, i.e. \"([]())\" or \"([)]\" (default ([)])")
+# read input from user
+user_input = "([)]"
+
+user_input = input().strip()
+if user_input:
+    input = user_input
+else:
+    input = "([)]"
+
+# create two stacks and a broken flag
+pstack = []
+bstack = []
+broken = False
+
+for i in range(len(input)):
+    char = input[i]
+    if char == '(':
+        # push to pstack
+        pstack.append(len(bstack)) # store current bstack size
+    elif char == ')':
+        # pop from pstack if not empty
+        if pstack:
+            if pstack and pstack[-1] > len(bstack): # check if brackets are trapped inside parentheses
+                broken = True
+                break
+            pstack.pop()
+        else:
+            broken = True
+            break
+    elif char == '[':
+        # push to bstack
+        bstack.append(len(pstack)) # store current pstack size
+    elif char == ']':
+        # pop from bstack if not empty
+        if bstack:
+            if bstack and bstack[-1] > len(pstack): # check if parentheses are trapped inside brackets
+                broken = True
+                break
+            bstack.pop()
+        else:
+            broken = True
+            break
+
+if broken or pstack or bstack:
+    print("NOT OK!")
+else:
+    print("OK!")
